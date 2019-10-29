57e5745d491a0bf666695d8eb6ac3421634a0399
Matthew Coombs <matthew_k_coombs@outlook.com>
2019-10-28 20:03:32 -0400

    Initial commit

---
+++ domain_description_crawler.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import itertools
+import argparse
+from bs4 import BeautifulSoup
+
+
+#Enter a list of domains user the format "domain.com"
+domains = ['www.goal.com']
+
+for domain in domains:
+
+    try:
+        crawl_domain = 'http://www.' + domain
+        response = requests.get(crawl_domain)
+        text = response.text
+        soup = BeautifulSoup(text, features= 'lxml')
+
+        #This block sifts through the DOM of the domain for the <meta> data
+        #description tag of the website
+        metas = soup.find_all('meta')
+        meta_data = [ meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ]
+
+        if len(meta_data) < 1:
+            print('No description can be pulled for ' + domain)
+            print('Please go to the actual site')
+        
+        else:
+            print('\n' + domain + ' description:\n')
+            print(meta_data[0] +  '\n')
+    
+    except Exception as error:
+        print(error)
+        print('Crawl FAILED: Please try the naming convention of "domain.com"')
+        continue
+
+print('Done')
